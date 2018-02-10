d116b4b873d121b9fdba68721683971dd84da205
Hironori Akaishi <ah.gm3622@gmail.com>
2018-02-10 14:56:05 +0900

    投稿バリデーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,10 @@ class Post < ApplicationRecord
   has_many :tags, through: :post_tags
   has_many :likes, dependent: :destroy
 
+  validates :post_image, presence: true
+  validates :place_id, presence: true
+  validates :category_id, presence: true
+
   def isLike_user?(user_id)
     likes.find_by(user_id: user_id).present?
   end
